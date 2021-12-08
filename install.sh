b801908a40c563a0ee08d83e967ebc0278d098df
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2021-12-08 12:12:44 +0100

    Update install.sh
    Fixed the location the script looks for bash

--- install.sh
+++ install.sh
@@ -1,21 +1,27 @@
-bash="/usr/bin/bash"
+#declare where to look for a shell
+bash="bin/bash"
 zsh="/usr/bin/zsh"
 
+#copy the script in the /usr/bin folder
 sudo cp $PWD/PrintNeofetchOnShell.sh /usr/bin/PrintNeofetchOnShell.sh
+
+#give it the right permissions
 sudo chmod 667 /usr/bin/PrintNeofetchOnShell.sh
 
+#create a backup folder to backup old files
 mkdir oldBashZshBackup
-cp ~/.bashrc oldBashZshBackup
-cp ~/.zshrc oldBashZshBackup
 
 if [[ "$SHELL" == "$bash" ]]; then
-  echo "/usr/bin/PrintNeofetchOnShell.sh" >> ~/.bashrc
-  source ~/.bashrc
+  cp ~/.bashrc oldBashZshBackup #make a backup
+  echo "/usr/bin/PrintNeofetchOnShell.sh" >> ~/.bashrc #tell bash to run the script when opened
+  source ~/.bashrc #update the bashrc so no reboot is needed
 fi
 
 if [[ "$SHELL" == "$zsh" ]]; then
-  echo "/usr/bin/PrintNeofetchOnShell.sh" >> ~/.zshrc
-  source ~/.zshrc
+  cp ~/.zshrc oldBashZshBackup #make a backup
+  echo "/usr/bin/PrintNeofetchOnShell.sh" >> ~/.zshrc #tell zsh to run the script when opened
+  source ~/.zshrc #update the zshrc so no reboot is needed
 fi
+
 
 /usr/bin/PrintNeofetchOnShell.sh
